633f99910ac13af8ab67ecd96571f77018e8254c
PabloPayDev <PabloJBecerraVDev@gmail.com>
2024-10-30 14:00:30 -0400

    Pablo - Meta api conection

--- app.py
+++ app.py
@@ -2,20 +2,28 @@ from flask import Flask, jsonify, request
 from pymongo import MongoClient
 from bson import ObjectId
 import pandas as pd
+from datetime import datetime
+import http.client
+import json
+import logging
 
 app = Flask(__name__)
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['meta_db_100J']
 
-def json_serializer(data):
-    if isinstance(data, ObjectId):
-        return str(data)
-    if isinstance(data, list):
-        return [json_serializer(item) for item in data]
-    if isinstance(data, dict):
-        return {key: json_serializer(value) for key, value in data.items()}
-    return data
+metaToken = "EAAHjoggR6icBO3JYyQiMY7nmCeoYhe6UGi5KPyeG0KnfCfWACDmgKmyZBC0Ul162cTcsPWNFdQadpmx88cDEZCoVNpWbxWZAigFd4SalIn1UeQTYXZACSKJvVFCSW6KxBPdtZAWQzVaLsU9EEgXZBzoEsUjdUZAdpuFXdIgRRl1ezvhg61rGU9JsHW8b6632qTJDIhZCKeZAtaovF6sTbndTNg5xq"
+webhookToken = "CHATBOTTOKENTEST"
+
+# ======= ======= ======= ROUTING SECTION ======= ======= =======
+@app.route('/webhook', methods=['GET','POST'])
+def webhook():
+    if(request.method == 'GET'):
+        challenge = verificar_token(request)
+        return challenge
+    elif(request.method == 'POST'):
+        response = recibir_mensaje(request)
+        return response
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
@@ -29,7 +37,140 @@ def add_data():
     collection = db['data']
     collection.insert_one(new_data)
     return jsonify(json_serializer(new_data)), 201
+# ======= ======= ======= ======= ======= ======= =======
+# ======= ======= ======= SOME FUNCTIONS SECTION ======= ======= =======
+def json_serializer(data):
+    if isinstance(data, ObjectId):
+        return str(data)
+    if isinstance(data, list):
+        return [json_serializer(item) for item in data]
+    if isinstance(data, dict):
+        return {key: json_serializer(value) for key, value in data.items()}
+    return data
 
+def verificar_token(req):
+    token = req.args.get('hub.verify_token')
+    challenge = req.args.get('hub.challenge')
+
+    if(challenge and (token == webhookToken)):
+        return challenge
+    else:
+        return jsonify({'error':'Token Invalido'}),401
+
+def recibir_mensaje(req):
+    try:
+        req = request.get_json()
+
+        entry = req["entry"][0]
+        changes = entry["changes"][0]
+        value = changes["value"]
+        objeto_mensaje = value["messages"]
+
+        if(objeto_mensaje):
+            messages = objeto_mensaje[0]
+            if("type" in messages):
+                tipo = messages["type"]
+                addMessage(json.dumps(messages))
+
+                if(tipo == "interactive"):
+                    tipo_interactivo = messages["interactive"]["type"]
+                    if(tipo_interactivo == "button_reply"):
+                        text = messages["interactive"]["button_reply"]["id"]
+                        numero = messages["from"]
+
+                        enviar_mensajes_whatsapp(text, numero)
+
+                    if(tipo_interactivo == "list_reply"):
+                        text = messages["interactive"]["list_reply"]["id"]
+                        numero = messages["from"]
+
+                        enviar_mensajes_whatsapp(text, numero)
+
+                if("text" in messages):
+                    text = messages["text"]["body"]
+                    numero = messages["from"]
+
+                    enviar_mensajes_whatsapp(text, numero)
+
+        return jsonify({'message':'EVENT RECEIVED'})
+
+    except Exception as e:
+        app.logger.debug('Error: Recibir mensaje')
+        return jsonify({'message':'EVENT RECEIVED'})
+
+def enviar_mensajes_whatsapp(texto, numero):
+    global flowStep
+
+    if(("test") in (texto.lower())):
+        flowStep = 1
+        data = {
+            "messaging_product": "whatsapp",    
+            "recipient_type": "individual",
+            "to": numero,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": "Hola, Bienvenido"
+            }
+        }
+    elif(("hola" in (texto.lower()))and(flowStep==0)):
+        data = {
+            "messaging_product": "whatsapp",    
+            "recipient_type": "individual",
+            "to": numero,
+            "type": "interactive",
+            "interactive": {
+                "type": "button",
+                "body":{
+                    "text": chatbotFlowMessages[0][0]
+                },
+                "footer":{
+                    "text": chatbotFlowMessages[0][1]
+                },
+                "action":{
+                    "buttons":[
+                        {
+                            "type": "reply",
+                            "reply":{
+                                "id": chatbotFlowMessages[0][2][0],
+                                "title": chatbotFlowMessages[0][2][1]
+                            }
+                        }
+                    ]                    
+                }                
+            }
+        }
+
+    else:
+        data = {
+            "messaging_product": "whatsapp",    
+            "recipient_type": "individual",
+            "to": numero,
+            "type": "text",
+            "text": {
+                "preview_url": False,
+                "body": chatbotFlowMessages[5][0]
+            }
+        }
+
+    data = json.dumps(data)
+    headers = {
+        "Content-Type" : "application/json",
+        "Authorization": "Bearer "+metaToken
+    }
+
+    connection = http.client.HTTPSConnection("graph.facebook.com")
+
+    try:
+        connection.request("POST", "/v20.0/374877792366425/messages", data, headers)
+        response = connection.getresponse()
+        print(response.status, response.reason)
+    except Exception as e:
+        addMessageLog(json.dumps(e))
+    finally:
+        connection.close()
+# ======= ======= ======= ======= ======= ======= =======
+# ======= ======= ======= APP INIT SECTION ======= ======= =======
 if __name__ == '__main__':
     #app.run(debug=True)
     data = [
@@ -41,3 +182,4 @@ if __name__ == '__main__':
 
     df = pd.DataFrame(data[1:], columns=data[0])
     df.to_excel('datos.xlsx', index=False)
+# ======= ======= ======= ======= ======= ======= =======
